db29b7f2750ad253c67aabce0674cf94e12cee7a
JostinAlvarado552 <jealvarado2@utpl.edu.ec>
2022-01-25 00:49:40 -0500

    primer commir

--- Ejercicios/src/ejercicios/Problema03.java
+++ Ejercicios/src/ejercicios/Problema03.java
@@ -16,6 +16,29 @@ public class Problema03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         double[] datos1 = {100, 90, 80, 70, 60};
+        double[] datos2 = {200, 190, 180, 170, 160};
+        double[] suma = new double[5];
+
+        for (int i = 0; i < datos1.length; i++) {
+
+            double resultado = obtenerSuma(datos1[i], datos2[i]);
+            suma[i] = resultado;
+
+            System.out.printf("La suma de %.2f + %.2f es: %.2f\n",
+                    datos1[i], datos2[i], suma[i]);
+        }
+
+    }
+
+    public static double obtenerSuma(double a, double b) {
+        if (b == 0) {
+            return a;
+        
+            } else {
+                return 1 + obtenerSuma(a, b - 1);
+            }
+        }
     }
     
-}
+
--- Ejercicios/src/ejercicios/Problema04.java
+++ Ejercicios/src/ejercicios/Problema04.java
@@ -16,6 +16,38 @@ public class Problema04 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[] numero = {100, 90, 80, 70, 60};
+        double[] potencia = {2, 3, 2, 2, 1};
+        double[] resultado = new double[5];
+        String cadena_final = "";
+        cadena_final = String.format("%sResultado de Potencias: \n", cadena_final);
+        for (int i = 0; i < numero.length; i++) {
+            resultado[i] = potencia(numero[i], potencia[i]);
+            cadena_final = String.format("%s%.2f\n",
+                    cadena_final, resultado[i]);
+        }
+        cadena_final = String.format("%sSuma obtenida de las potencias: \n%.2f\n",
+                cadena_final, suma(resultado));
+        System.out.printf("%s", cadena_final);
+    }
+
+    public static double potencia(double numero, double exponente) {
+        if (exponente == 1) {
+            return numero;
+        } else {
+            return numero * potencia(numero, exponente - 1);
+        }
+    }
+
+    public static double suma(double[] arreglo) {
+        double suma = 0;
+        for (int i = 0; i < arreglo.length; i++){
+            suma = suma + arreglo[i];
+        }
+        return suma;
     }
     
 }
+    
+    
+
